4be974829271f6593e4d5f663f55388d162ee986
blued_gear <chocolatecakecodes@disroot.org>
2023-09-30 18:02:47 +0200

    add task to publish UserScript

--- UserScripts/build.gradle.kts
+++ UserScripts/build.gradle.kts
@@ -1,9 +1,17 @@
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.file.Files
+import java.util.*
+
 plugins {
     id("io.github.pereduromega.npm.plugin") version "1.3.0"
 }
 
 group = "apps.chocolatecakecodes.invidious_ext"
-version = "1.0-SNAPSHOT"
+version = "1.0-Beta"
 
 repositories {
     mavenCentral()
@@ -29,4 +37,48 @@ tasks.named<NpmScriptTask>("build") {
     inputs.file(project.file("package.json"))
     inputs.file(project.file("vite.config.ts"))
     outputs.dir("dist")
+}
+
+tasks.create("publishUserScript") {
+    group = "publish"
+    dependsOn.add("build")
+
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
+
+    val filename = "invidious-extension.user.js"
+    val url = "${properties.getProperty("gitlab.host")}/api/v4/projects/${properties.getProperty("gitlab.projectId")}/packages/generic/${project.name}/${project.version}/$filename"
+    val file = java.nio.file.Path.of("${project.projectDir}/dist/$filename")
+    val authCredentials = "${properties.getProperty("gitlab.publish.username")}:${properties.getProperty("gitlab.publish.password")}"
+
+    doLast {
+        (URL(url).openConnection() as HttpURLConnection).apply {
+            doOutput = true
+            doInput = true
+            instanceFollowRedirects = true
+            requestMethod = "PUT"
+            setRequestProperty("Authorization", "Basic ${Base64.getEncoder().encodeToString(authCredentials.toByteArray())}")
+        }.also { http ->
+            val httpOut = http.outputStream
+            val fileIn = Files.newInputStream(file)
+
+            fileIn.transferTo(httpOut)
+
+            httpOut.flush()
+            httpOut.close()
+            fileIn.close()
+        }.also { http ->
+            val httpRespCode = http.responseCode
+            if(httpRespCode < 200 || httpRespCode >= 300) {
+                val respLines = BufferedReader(InputStreamReader(http.errorStream)).use {
+                    it.readLines()
+                }
+
+                val msg = "Upload of debug apk failed: statusCode = $httpRespCode; statusMessage = ${http.responseMessage}\n${respLines.joinToString("\n")}"
+                throw IOException(msg)
+            } else {
+                logger.info("Uploaded debug apk")
+            }
+        }
+    }
 }
