f3a1f52e2b387c99c943cb3ab07ae47fca9a59e1
blued_gear <chocolatecakecodes@disroot.org>
2023-10-09 21:33:12 +0200

    Script: more fixes for the Base64-workaround

--- UserScripts/src/workarounds/base64.ts
+++ UserScripts/src/workarounds/base64.ts
@@ -4,10 +4,22 @@ import {STR_ENCODER} from "../util/constants";
 
 //XXX there is a browser-restriction in Firefox that blocks access to TypedArrays cross-context
 //  this breaks Base64.fromUint8Array()
+//  also CSP may block cross-context access to TypedArrays
 
 export function base64FromArrayBuffer(data: ArrayBuffer): string {
-    const dataBytes = new unsafeWindow.Uint8Array(data);
-    return Base64.fromUint8Array(dataBytes);
+    let dataBytes: Uint8Array;
+    try {
+        dataBytes = new unsafeWindow.Uint8Array(data);
+    } catch (ignored) {
+        dataBytes = new window.Uint8Array(data);
+    }
+
+    const dataCpy = new ArrayBuffer(data.byteLength);
+    const dataCpyBytes = new Uint8Array(dataCpy);
+    for(let i = 0; i < dataBytes.length; i++)
+        dataCpyBytes[i] = dataBytes[i];
+
+    return Base64.fromUint8Array(dataCpyBytes, true);
 }
 
 export function base64FromString(data: string): string {
