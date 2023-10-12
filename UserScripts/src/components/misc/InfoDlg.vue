1195bdda591e8748f3b1615b69e946f6abc67318
blued_gear <chocolatecakecodes@disroot.org>
2023-10-12 15:10:03 +0200

    Script::InfoDlg: improve styling

--- UserScripts/src/components/misc/InfoDlg.vue
+++ UserScripts/src/components/misc/InfoDlg.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import {computed, ref} from "vue";
 import Dialog from "primevue/dialog";
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
@@ -6,15 +7,20 @@ import TabPanel from 'primevue/tabpanel';
 import contentLicensesBackend from "../../assets/licenses/backend.html?raw";
 import contentLicensesFrontend from "../../assets/licenses/frontend.html?raw";
 
-const show = defineModel<boolean>()
+const show = defineModel<boolean>();
+
+const contentRef = ref<HTMLElement>();
+
+const contentHeight = computed(() => contentRef.value?.clientHeight ?? 0);
 </script>
 
 <template>
-  <Dialog v-model:visible="show" :closable="true" :blockScroll="true" header="Info">
-    <div style="width: 90vw; height: 80vh;">
+  <Dialog v-model:visible="show" :closable="true" :blockScroll="true" header="Info"
+          content-class="h-full" style="width: 90vw; height: 80vh;">
+    <div ref="contentRef" class="w-full h-full">
       <TabView class="h-full">
         <TabPanel header="Info">
-          <div>
+          <div class="w-full" :style="`height: ${contentHeight}px;`">
             TODO
 
             <p>
@@ -23,10 +29,10 @@ const show = defineModel<boolean>()
           </div>
         </TabPanel>
         <TabPanel header="Backend Licenses">
-          <div v-html="contentLicensesBackend"></div>
+          <iframe :srcdoc="contentLicensesBackend" class="border-none w-full" :style="`height: ${contentHeight}px;`"></iframe>
         </TabPanel>
-        <TabPanel header="Frontend Licenses">
-          <div v-html="contentLicensesFrontend"></div>
+        <TabPanel header="Frontend Licenses" :style="{ height: `${contentHeight}px` }">
+          <iframe :srcdoc="contentLicensesFrontend" class="border-none w-full" :style="`height: ${contentHeight}px;`"></iframe>
         </TabPanel>
       </TabView>
     </div>
