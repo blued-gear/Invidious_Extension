b290bf797bc36b82a6a9681d675e1d4d6ebb379b
blued_gear <chocolatecakecodes@disroot.org>
2024-04-17 19:32:57 +0200

    Script: fix main_menu-player "Reverse Playlist"

--- UserScripts/src/components/main-menu/entries/player.ts
+++ UserScripts/src/components/main-menu/entries/player.ts
@@ -1,10 +1,10 @@
 import {MenuItem} from "primevue/menuitem";
 import urlExtractor from "../../../controllers/url-extractor";
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import playerManager from "../../../managers/player";
 
 const reversePlaylist = ref(false);
-let reversePlaylistItemClass = "menuItem-checkbox-unchecked";
+let reversePlaylistItemClass = ref("menuItem-checkbox-unchecked");
 
 function toggleReversePlaylist() {
     playerManager.setReversePlaylist(!reversePlaylist.value);
@@ -14,14 +14,14 @@ function toggleReversePlaylist() {
 export function updateMenu() {
     const enabled = playerManager.isReversePlaylist();
     reversePlaylist.value = enabled;
-    reversePlaylistItemClass = enabled ? "menuItem-checkbox-checked" : "menuItem-checkbox-unchecked";
+    reversePlaylistItemClass.value = enabled ? "menuItem-checkbox-checked" : "menuItem-checkbox-unchecked";
 }
 
-export default () => <MenuItem[]>[
+export default () => reactive<MenuItem[]>([
     {
         label: "Reverse Playlist",
         command: () => toggleReversePlaylist(),
         visible: () => urlExtractor.isPlayingPlaylist(),
-        class: "menuItem-checkbox " + reversePlaylistItemClass
+        class: "menuItem-checkbox " + reversePlaylistItemClass.value
     }
-];
+]);
